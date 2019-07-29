582000df8a17b4b07c255c1df64b80ea8b9ba953
Aaron-zheng <fucheng.zheng@gmail.com>
2019-07-30 02:50:34 +0800

    初始化项目

---
+++ functionimage/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  functionimage
+//
+//  Created by Aaron on 30/7/2019.
+//  Copyright © 2019 sightcorner. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ functionimage/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  functionimage
+//
+//  Created by Aaron on 30/7/2019.
+//  Copyright © 2019 sightcorner. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
